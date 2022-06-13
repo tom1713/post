ad7ec5dd7e864f8bb154c58b7f9242438c636263
tom1713 <tom1713@hotmail.com>
2022-06-13 19:10:06 +0800

    second deploy

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #初始化 flask 伺服器
 from flask import *
 import os
-# from model.member import mb
-# from model.register import regs
-# from model.signin import sin
-# from model.error import er
-# from model.signout import so
-# from model.post import ps
-# from model.db import db
+from model.member import mb
+from model.register import regs
+from model.signin import sin
+from model.error import er
+from model.signout import so
+from model.post import ps
+from model.db import db
 
 app = Flask(
     __name__,
@@ -17,12 +17,12 @@ app = Flask(
 
 app.secret_key = "123"
 
-# app.register_blueprint(mb)
-# app.register_blueprint(regs)
-# app.register_blueprint(sin)
-# app.register_blueprint(ps)
-# app.register_blueprint(er)
-# app.register_blueprint(so)
+app.register_blueprint(mb)
+app.register_blueprint(regs)
+app.register_blueprint(sin)
+app.register_blueprint(ps)
+app.register_blueprint(er)
+app.register_blueprint(so)
 
 #處理路由
 @app.route("/")
