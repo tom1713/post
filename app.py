71277096370dc0f7c4a7847e151a1d4673b7db73
tom1713 <tom1713@hotmail.com>
2022-06-13 15:56:39 +0800

    first deploy

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-from flask import *
-"""藍圖物件可看做一個縮小版的app物件"""
-
-import time, json
-#初始化資料庫連線
-import pymongo
-client = pymongo.MongoClient("mongodb+srv://root:root123@cluster0.hpcbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.member_system
-print("資料庫連線建立成功")
-
 #初始化 flask 伺服器
 from flask import *
-from model.member import mb
-from model.register import regs
-from model.signin import sin
-from model.error import er
-from model.signout import so
-from model.post import ps
-from model.db import db
+# from model.member import mb
+# from model.register import regs
+# from model.signin import sin
+# from model.error import er
+# from model.signout import so
+# from model.post import ps
+# from model.db import db
 
 app = Flask(
     __name__,
@@ -26,12 +16,12 @@ app = Flask(
 
 app.secret_key = "123"
 
-app.register_blueprint(mb)
-app.register_blueprint(regs)
-app.register_blueprint(sin)
-app.register_blueprint(ps)
-app.register_blueprint(er)
-app.register_blueprint(so)
+# app.register_blueprint(mb)
+# app.register_blueprint(regs)
+# app.register_blueprint(sin)
+# app.register_blueprint(ps)
+# app.register_blueprint(er)
+# app.register_blueprint(so)
 
 #處理路由
 @app.route("/")
--- model/signout.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-"""藍圖物件可看做一個縮小版的app物件"""
-so = Blueprint('so', __name__) # 第一個引數為藍圖名稱，隨便取
-
-@so.route("/signout")
-def signout():
-    #移除session中的會員資訊
-    del session["nickname"]
-    return redirect("/")
