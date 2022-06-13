91e571e79fb7c9f364808a46c6d6309c13ceb684
tom1713 <tom1713@hotmail.com>
2022-06-13 16:52:41 +0800

    first deploy

---
+++ model/db.py
@@ -0,0 +1,9 @@
+from flask import *
+"""藍圖物件可看做一個縮小版的app物件"""
+
+import time, json
+#初始化資料庫連線
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://root:root123@cluster0.hpcbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.member_system
+print("資料庫連線建立成功")
