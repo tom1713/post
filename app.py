45f0c5fd9ca6fe8e072a57334ac85e336a79a927
tom1713 <tom1713@hotmail.com>
2022-06-14 11:42:03 +0800

    second deploy

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ def index():
     return render_template("index.html")
 
 #啟動伺服器
-port = int(os.environ.get('PORT', 0))
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 0))
     app.run(host='0.0.0.0', port=port, debug=True)
