d088086bbbfe79537beaf209d2bcaad714c91f91
tom1713 <tom1713@hotmail.com>
2022-06-14 11:51:46 +0800

    second deploy

--- model/db.py
+++ model/db.py
@@ -1,6 +1,6 @@
 import time, json
 #初始化資料庫連線
 import pymongo
-client = pymongo.MongoClient("mongodb://root:root123@cluster0.hpcbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", SSL=False, tls=True)
+client = pymongo.MongoClient("mongodb://root:root123@cluster0.hpcbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", SSL=False)
 db = client.member_system
 print("資料庫連線建立成功")
