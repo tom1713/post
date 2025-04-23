dd0ec9c7008bd88cc9a663deb7f07e5823f5f525
tom1713 <69420558+tom1713@users.noreply.github.com>
2025-04-23 14:54:47 +0800

    更新mongodb conntion

--- model/db.py
+++ model/db.py
@@ -1,6 +1,6 @@
 import time, json
 #初始化資料庫連線
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://root:root123@cluster0.hpcbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://user:user123@cluster0.hpcbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.member_system
 print("資料庫連線建立成功")
